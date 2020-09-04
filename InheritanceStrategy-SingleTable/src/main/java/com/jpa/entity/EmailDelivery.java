48813fa65970bfa98a3dc4c9cf9bb8fc3836f89e
Abhijeet Ramesh Kulkarni <kulkarniabhijeet17@gmail.com>
2020-09-04 14:17:37 +0530

    Single table inheritance strategy example.

---
+++ InheritanceStrategy-SingleTable/src/main/java/com/jpa/entity/EmailDelivery.java
@@ -0,0 +1,25 @@
+package com.jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "email_delivery_tbl")
+@DiscriminatorValue("ED")
+public class EmailDelivery extends UserInfo {
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "email_address")
+	private String emailAddress;
+
+	// getters and setters
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+}
---
+++ InheritanceStrategy-SingleTable/src/main/java/com/jpa/entity/PhysicalDelivery.java
@@ -0,0 +1,45 @@
+package com.jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "physical_delivery_tbl")
+@DiscriminatorValue("PD")
+public class PhysicalDelivery extends UserInfo {
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "address_line1")
+	private String addressLine1;
+	@Column(name = "address_line2")
+	private String addressLine2;
+	@Column(name = "zip_code")
+	private int zipCode;
+
+	// getters and setters
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+}
